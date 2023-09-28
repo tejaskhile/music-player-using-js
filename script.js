b3220f7514f3145f465e9a40dda2c0cc8cca5057
Tejas Khile <128159889+tejaskhile@users.noreply.github.com>
2023-09-28 16:11:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,3 @@
-// let progress = document.querySelector(".progressBar");
-// let song = document.querySelector(".song");
-// let ctrlIcon = document.querySelector("#ctrlIcon");
-
-// song.onloadedmetadata = ()=>{
-//     progress.max = song.duration;
-//     progress.value = song.currentTime;
-// }
-
-// function playPause(){
-//     if(ctrlIcon.classList.contains('fa-pause')){
-//         song.pause();
-//         ctrlIcon.classList.remove('fa-pause');
-//         ctrlIcon.classList.add('fa-play');
-//     }else{
-//         song.play();
-//         ctrlIcon.classList.add('fa-pause');
-//         ctrlIcon.classList.remove('fa-play');
-//     }
-// }
-// if(song.play()){
-//     setInterval(()=>{
-//         progress.value = song.currentTime;
-//     }, 500)
-// }
-
-// progress.onchange= ()=>{
-//     song.play();
-//     song.currentTime = progress.value;
-
-//     ctrlIcon.classList.add('fa-pause');
-//         ctrlIcon.classList.remove('fa-play');
-// }
-
-
 const songs = [
     {
         name : 'Chaleya',
@@ -79,8 +44,8 @@ let currentIndexSong = 0;
 const loadSongs = (songs)=>{
     songName.innerHTML = songs.name;
     artistName.innerHTML = songs.artist;
-    audio.src = "./assets/"+ songs.src;
-    image.src = "./assets/"+ songs.cover;
+    audio.src = "./images/"+ songs.src;
+    image.src = "./images/"+ songs.cover;
 }
 
 loadSongs(songs[currentIndexSong]);
